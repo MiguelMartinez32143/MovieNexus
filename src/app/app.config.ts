b47fbfbe060b3e7fc8d05349ffa7fc708183e8de
MiguelMartinez32143 <miguelmartinezvaron32143@gmail.com>
2026-05-21 15:21:21 -0500

    feat: agregar transiciones de página fluidas estilo iPhone (abrir y cerrar app) con rebote

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(
       withFetch(), // Habilita el uso de la API Fetch moderna (más rápida y compatible con SSR)
       withInterceptors([apiInterceptor]) // Registra nuestro interceptor (Paso 3)
