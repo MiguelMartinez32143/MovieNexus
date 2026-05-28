586b23f16ddbfd64d6f269a9b71ff9e8a7e91297
MiguelMartinez32143 <miguelmartinezvaron32143@gmail.com>
2026-05-28 15:07:52 -0500

    feat: configure PWA support, install service worker, and generate premium icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,10 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withFetch(), // Habilita el uso de la API Fetch moderna (más rápida y compatible con SSR)
       withInterceptors([apiInterceptor, errorInterceptor]) // Registra nuestros interceptores (Paso 3)
-    )
+    ),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
